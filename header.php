7ad9213bfc2ac6951f123cf087615064baa2c7c0
Phillip Pan <phillip.y.pan@gmail.com>
2014-11-24 17:05:36 -0600

    fix indenting

--- header.php
+++ header.php
@@ -6,43 +6,44 @@
     <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300' rel='stylesheet' type='text/css'>  
   </head>
   <body>
-  	<div id="menu">
+    <div id="menu">
   	  <nav class="nav">
-  	  	<ul id="nav-list">
+  	    <ul id="nav-list">
   	  	  <li id="home">
-  	  	  	<a href="index.php">HOME</a>
-  	  	  </li>
-  	  	  <li id="kana">
-  	  	  	<a href="kana.php">KANA</a>
-  	  	  </li>
-  	  	  <li id="kanji">
-  	  	  	<a href="kanji.php">KANJI</a>
-  	  	  </li>
-  	  	  <li id="vocabulary">
-  	  	  	<a href="vocabulary.php">VOCABULARY</a>
-  	  	  </li>
-  	  	  <li id="grammar">
-  	  	  	<a href="grammar.php">GRAMMAR</a>
-  	  	  </li>
-  	  	  <li id="counters">
-  	  	  	<a href="counters.php">COUNTERS</a>
-  	  	  </li>
-  	  	  <li id="reading">
-  	  	  	<a href="reading.php">READING</a>
-  	  	  </li>
-  	  	  <li id="listen">
-  	  	  	<a href="listen.php">LISTEN/SPEAK</a>
-  	  	  </li>
-  	  	  <li id="culture">
-  	  	  	<a href="culture.php">CULTURE</a>
-  	  	  </li>
-  	  	  <li id="links">
-  	  	  	<a href="links.php">LINKS</a>
-  	  	  </li>
-  	  	</ul>
-  	 </div>
+  	  	    <a href="index.php">HOME</a>
+          </li>
+          <li id="kana">
+            <a href="kana.php">KANA</a>
+          </li>
+          <li id="kanji">
+            <a href="kanji.php">KANJI</a>
+          </li>
+          <li id="vocabulary">
+            <a href="vocabulary.php">VOCABULARY</a>
+          </li>
+          <li id="grammar">
+            <a href="grammar.php">GRAMMAR</a>
+          </li>
+          <li id="counters">
+            <a href="counters.php">COUNTERS</a>
+          </li>
+          <li id="reading">
+            <a href="reading.php">READING</a>
+          </li>
+          <li id="listen">
+            <a href="listen.php">LISTEN/SPEAK</a>
+          </li>
+          <li id="culture">
+            <a href="culture.php">CULTURE</a>
+          </li>
+          <li id="links">
+            <a href="links.php">LINKS</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
-     <div id="main-text">
+    <div id="main-text">
       <div id="body-paragraph">
         <p id="logo">
         <span class="bold green">J</span>APANESE <span class="bold green">O</span>NLINE <span class="bold green">S</span>ELF <span class="bold green">H</span>ELP <span class="bold green">U</span>TILITY
